3b896d75196f6d46259cb1914741dd4485b0aaa5
tkrenato <tkrenato@gmail.com>
2023-05-10 01:28:17 +0000

    update postgresql file

--- postgresql_fake_data.py
+++ postgresql_fake_data.py
@@ -24,17 +24,30 @@ cur.close()
 conn.close()
 
 # Connect to the `sampledb` database
-conn = psycopg2.connect("host=localhost dbname=sampledb user=postgres password=welcome1")
+conn = psycopg2.connect(
+    host="localhost",
+    database="sampledb",
+    user="postgres",
+    password="welcome1"
+    )
 
 # Create a cursor object
 cur = conn.cursor()
 
 # Create the `sampletable1` table
-cur.execute("CREATE TABLE sampletable1 (id INT, name VARCHAR(255), email VARCHAR(255), lastupdate TIMESTAMP)")
+create_table = """
+    CREATE TABLE sampletable1 (
+        id INT,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        lastupdate TIMESTAMP
+    )
+"""
+cur.execute(create_table)
 
 # Generate 1GB of data inside the `sampletable1` table
 fake = Faker()
-for i in range(10000000):
+for i in range(1000):
     cur.execute("INSERT INTO sampletable1 (id, name, email, lastupdate) VALUES (%s, %s, %s, %s)", (i, fake.name(), fake.email(), fake.date_time()))
 
 # Commit the changes to the database
